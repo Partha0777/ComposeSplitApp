2cf36433ff3501fa5694e18815d64fa3eac1a9c3
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-07-20 20:27:15 +0530

    Added blank header function.

--- app/src/main/java/com/curiozing/composesplitapp/MainActivity.kt
+++ app/src/main/java/com/curiozing/composesplitapp/MainActivity.kt
@@ -3,14 +3,21 @@ package com.curiozing.composesplitapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.curiozing.composesplitapp.ui.theme.ComposeSplitAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,8 +39,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HeaderView(total: Double = 0.0) {
-    Card {
-    }
+   Card(modifier = Modifier.height(150.dp).fillMaxWidth().background(color = Color.Gray)) {
+
+   }
 }
 
 @Composable
