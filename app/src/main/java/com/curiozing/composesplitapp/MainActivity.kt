89e1086641407b1ce6fcd3fc48d898040c00ad10
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-07-22 23:01:37 +0530

    Added text view in the header view.

--- app/src/main/java/com/curiozing/composesplitapp/MainActivity.kt
+++ app/src/main/java/com/curiozing/composesplitapp/MainActivity.kt
@@ -3,23 +3,24 @@ package com.curiozing.composesplitapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.curiozing.composesplitapp.ui.theme.ComposeSplitAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -55,6 +56,11 @@ fun HeaderView() {
             .fillMaxWidth()
             .padding(horizontal = 12.dp, vertical = 12.dp)
     ) {
+
+        Box(
+            modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+            Text(text = "$100", style = TextStyle(fontSize = 28.sp, fontWeight = FontWeight(600)))
+        }
 
     }
 }
